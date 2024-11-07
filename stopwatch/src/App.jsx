2f7d9d64944a88c3cfb1f09e5ddac3f4a36d89ee
Ashwin <ashvinkumar8891@gmail.com>
2024-11-07 16:58:23 +0530

    Test commit- Displaying components

---
+++ stopwatch/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Stopwatch</h1>
+        <p>
+          Time:{" "}
+          <span>
+            <span>0</span> : <span>00</span>
+          </span>
+        </p>
+        <button type="button">Start</button>
+        <button type="button">Stop</button>
+        <button type="button">Reset</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
